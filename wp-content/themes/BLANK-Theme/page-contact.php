1b5ff2c8f069fa26bd118185c4e23bf9bcd96201
David Walker <dwalker89@fullsail.edu>
2013-11-04 18:01:44 -0500

    fixed search width, started contact page

---
+++ wp-content/themes/BLANK-Theme/page-contact.php
@@ -0,0 +1,34 @@
+<?php
+
+	/*
+		Template Name: Contact Page
+	*/
+
+
+?>
+
+<?php get_header(); ?>
+
+ <div id='main-content'>
+
+ 	<h1>Message Us!</h1>
+
+ 	<form id='contact'>
+ 		<label for="con_name">Name</label>
+		<input type="text" id="con_name" />
+
+		<label for="email">Email Address</label>
+		<input type="text" id="email" />
+
+		<label for="message">Message</label>
+		<textarea rows='10' cols='75' type="text" id="message"></textarea>
+
+		<input type='submit' value='Send' class='subbtn'> 
+
+ 	</form>
+
+ </div>
+
+ <?php get_sidebar('contact'); ?>
+
+ <?php get_footer(); ?>
---
+++ wp-content/themes/BLANK-Theme/sidebar-contact.php
@@ -0,0 +1,13 @@
+<aside  id='contact-sidebar'>
+
+    <h2>Contact Info</h2>
+
+    <div>
+        <ul>
+            <li>Twitter: <a href="https://twitter.com/Gamerreview1">Link</a></li>
+            <li>Facebook: <a href="https://www.facebook.com/Gamerreview1">Link</a></li>
+            <li>Phone: Coming soon!</li>
+        </ul>
+    </div>
+    
+</aside>
